c69531ad71d1aec5038faf3ae229fa51009cb8e9
Anton <tonissstar058@gmail.com>
2025-01-12 23:46:20 +0100

    events_single

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -35,4 +35,38 @@ export default function slider() {
             }
         });
     }
+
+
+
+    const eventsSlider = document.querySelectorAll('.events-slider') || null;
+
+    if (eventsSlider) {
+        eventsSlider.forEach((newSwiper) => {
+            const progressBar = newSwiper.querySelector('.events-slider .swiper-progressbar');
+            const nextButton = newSwiper.querySelector('.events-slider .swiper-button-next');
+            const prevButton = newSwiper.querySelector('.events-slider .swiper-button-prev');
+
+            if (newSwiper) {
+                const eventsSlider = new Swiper(newSwiper, {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                    loop: true,
+                    on: {
+                        slideChange: function () {
+                            const progress = this.progress;
+                            progressBar.style.width = `${progress * 100}%`;
+                        },
+                    },
+                    navigation: {
+                        nextEl: nextButton,
+                        prevEl: prevButton,
+                    },
+                    pagination: {
+                        el: progressBar,
+                        type: 'progressbar',
+                    }
+                });
+            }
+        });
+    }
 }
